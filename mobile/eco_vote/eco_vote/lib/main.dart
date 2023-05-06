159195297971fa8340602116f2af148e106e5b9f
unknown <vasileios.alevizos@pm.me>
2023-05-06 23:08:45 +0300

    Flutter app.

---
+++ mobile/eco_vote/eco_vote/lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Eco Vote',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const Login(title: 'Eco Vote'),
+    );
+  }
+}
+
+
+class BottomNavigationExample extends StatefulWidget {
+  const BottomNavigationExample({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavigationExampleState createState() =>
+      _BottomNavigationExampleState();
+}
+
+class _BottomNavigationExampleState extends State {
+  int _selectedTab = 0;
+
+  List _pages = [
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("About"),
+    ),
+    Center(
+      child: Text("Products"),
+    ),
+  ];
+
+  _changeTab(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: _pages[_selectedTab],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (index) => _changeTab(index),
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "About"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.grid_3x3_outlined), label: "Product"),
+        ],
+      ),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Email"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Navigate the user to the Home page
+                        Navigator.push(
+                                  context,
+                              MaterialPageRoute(
+                              builder: (context) => BottomNavigationExample(
+        
+                              )),
+                              );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
